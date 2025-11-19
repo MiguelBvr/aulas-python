6b692e8826b3899fc475b76a0d9a21c992616a76
MiguelBvr <miguelbrv.ul@gmail.com>
2025-11-19 19:07:45 -0300

    Update exercios3_18.py

--- exercios3_18.py
+++ exercios3_18.py
@@ -2,11 +2,14 @@
 # itere sobre cada um dos valores da lista, converta esse valor para inteiro. Verifique se ele é maior que 0, caso seja conte ele, após a iteração retorne a quantidade de numeros contados. 
 numeros = input('Digite 10 numeros aleatorios ')
 numeros = numeros.split()
+
+quantidade_numero = 0
+
 for numero in numeros:
     numero = int(numero)
 
-    if numero < 0:
-        numero = 0
-    else:
-        numero = numero
-print(len(numeros))
+    if numero > 0:
+        quantidade_numero = quantidade_numero + 1      
+        
+print(quantidade_numero)
+        
