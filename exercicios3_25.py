35b8073459a73c7bc3065e30a5190de1b6facaa5
MiguelBvr <miguelbrv.ul@gmail.com>
2026-05-17 19:16:36 -0300

    Create exercicios3_25.py

---
+++ exercicios3_25.py
@@ -0,0 +1,19 @@
+from random import randint
+
+print('Digite o mesmo número que o computador de 1 a 100')
+
+numero_jogador = int(input('Digite seu número: '))
+
+numero_maquina = randint(1, 100)
+
+# print(numero_maquina)   # usei pra verificar a funcionalidade do código
+
+while numero_jogador != numero_maquina:
+
+    numero_jogador = int(input('Tente novamente: '))
+
+    if numero_jogador == numero_maquina:
+
+        print(f'Você acertou o número, que era {numero_maquina}')
+
+       
