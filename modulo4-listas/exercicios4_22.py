158a55ee1c80ad650314fcbfead6f5706570364e
MiguelBvr <miguelbrv.ul@gmail.com>
2026-09-18 09:53:09 -0300

    Update exercicios4_22.py

--- modulo4-listas/exercicios4_22.py
+++ modulo4-listas/exercicios4_22.py
@@ -3,7 +3,6 @@ itens = [{'Miguel':'pocao','qtd':3},{'Rafael':'espada','qtd':1}]
 total_itens = 0
 
 for item in itens:
-    if 'qtd' in item:
-        total_itens += item['qtd']
+    total_itens += item['qtd']
 
 print(total_itens)
